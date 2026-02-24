2ab14177f756f829c40b896b5157365c77b82ad9
stephen-otieno <stephenotieno8781@gmail.com>
2026-02-24 12:34:52 +0300

    allowed hosts

--- backend/local_servicehub/settings.py
+++ backend/local_servicehub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fiwt4r*7=hxt7-ont9qw!+u2fm2mooptf)agamm!a)*z%$dlkm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
